6b0c42dcf382721ba490f765c2717d95c5547dbc
Roshan Ghimire <53145171+roshan13ghimire@users.noreply.github.com>
2021-05-17 14:37:04 +0545

    Add files via upload

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP</title>
+</head>
+<body>
+    <?php
+        // echo "Hello World";
+
+        // echo "<h1>Hello World</h1>";
+
+        $name = "Roshan";
+
+        echo var_dump($name);    //checking datatype
+        // echo $name;
+
+        // $num1 = 10;
+        // $num2 = 20;
+
+        // $sum  = $num1 + $num2;
+
+        // echo "<h1>Sum of two numbers is $sum  </h1> ";
+
+
+
+    ?>
+</body>
+</html>
