2272463dbf769cf074fe2f7005070bcd5c5f3424
Roshan Ghimire <53145171+roshan13ghimire@users.noreply.github.com>
2021-05-19 13:13:51 +0545

    Add files via upload

---
+++ array.php
@@ -0,0 +1,8 @@
+<?php
+    $names = array("Roshan" , "Ravi" ,"Ashu" , "Aayush");
+
+    foreach ($names as $values)
+    {
+        echo $values ."<br>";
+    }
+?>
---
+++ function.php
@@ -0,0 +1,19 @@
+<?php
+    function printName($name){
+        return "Name:" .$name ."<br>";
+    }
+    echo printName("Roshan")
+
+?>
+
+
+
+<?php
+    function printName1(){
+
+        $name1 = "Ravi";
+        echo "Name:" .$name1;
+    }
+    printName1();
+
+?>
